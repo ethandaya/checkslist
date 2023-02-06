2675f4636fb37f40f53396dd1ce761641c014393
Ethan <me@ethandaya.com>
2023-02-07 01:09:31 +1100

    feat: working impl, getting preflight ready

---
+++ script/Deploy.s.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.10;
+
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+import "zora-drops-contracts/ZoraNFTCreatorV1.sol";
+import "zora-drops-contracts/ERC721Drop.sol";
+import {Strings} from "openzeppelin-contracts/utils/Strings.sol";
+import {CheckslistRenderer} from "../src/CheckslistRenderer.sol";
+
+contract Deploy is Script {
+    function run() public {
+        console.log("Starting ---");
+
+        ZoraNFTCreatorV1 creator = ZoraNFTCreatorV1(
+            payable(address(vm.envAddress("ZORA_NFT_CREATOR_ADDRESS")))
+        );
+
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        uint16 ROYALTY_BPS = 250;
+
+        ERC721Drop drop = ERC721Drop(
+            payable(address(vm.envAddress("EDITION_ADDRESS")))
+        );
+
+        CheckslistRenderer renderer = new CheckslistRenderer(
+            "Checkslist",
+            "This list may or may not be notable...or done.",
+            "ipfs:///bafybeibvflhws7clzdeqlpoie65dfgx62thoatry6oayv56loqheb3xqka",
+            Strings.toString(250),
+            string(abi.encodePacked(msg.sender)),
+            "https://checkslist.wtf",
+            payable(address(vm.envAddress("EDITION_ADDRESS")))
+        );
+
+        drop.setMetadataRenderer(renderer, "");
+
+        vm.stopBroadcast();
+    }
+}
--- test/CheckslistRenderer.t.sol
+++ test/CheckslistRenderer.t.sol
@@ -4,16 +4,16 @@ pragma solidity ^0.8.10;
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
 import {Strings} from "openzeppelin-contracts/utils/Strings.sol";
-import {ChecklistRenderer} from "../src/ChecklistRenderer.sol";
-import { ERC721AMock } from "erc721a/mocks/ERC721AMock.sol";
+import {CheckslistRenderer} from "../src/CheckslistRenderer.sol";
+import {ERC721AMock} from "erc721a/mocks/ERC721AMock.sol";
 
-contract ChecklistRendererTest is Test {
+contract CheckslistRendererTest is Test {
     ERC721AMock tokenContract;
-    ChecklistRenderer renderer;
+    CheckslistRenderer renderer;
 
     function setUp() public {
         tokenContract = new ERC721AMock("MOCK", "MCK");
-        renderer = new ChecklistRenderer(
+        renderer = new CheckslistRenderer(
             "Checklist",
             "descr",
             "image://",
@@ -26,13 +26,12 @@ contract ChecklistRendererTest is Test {
 
     function test_buildSVG() public {
         string memory svg = renderer.buildSVG(address(42069), 20);
-        assertEq(vm.readFile('./test/__snapshots__/test_buildSVG.txt'), svg);
+        assertEq(vm.readFile("./test/__snapshots__/test_buildSVG.txt"), svg);
     }
 
     function test_tokenURI() public {
-        vm.prank(address(42060));
         tokenContract.mint(msg.sender, 20);
         string memory uri = renderer.tokenURI(19);
-        assertEq(vm.readFile('./test/__snapshots__/test_tokenURI.txt'), uri);
+        assertEq(vm.readFile("./test/__snapshots__/test_tokenURI.txt"), uri);
     }
 }
